386e3bd2b5d259a4f425083b417b25ff6e6fee09
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-21 21:43:57 +0100

    add exercise 24

---
+++ week-02/day-02/exercise1-35/src/PrintEven.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Zita on 2017. 03. 21..
+ */
+public class PrintEven {
+  public static void main(String[] args) {
+    int a = 2;
+    while (a < 501) {
+      System.out.println(a);
+      a += 2;
+    }
+  }
+}
