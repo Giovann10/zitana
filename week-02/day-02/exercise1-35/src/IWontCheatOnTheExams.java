da745aef406107bb8fedadbff07f2c45a250e349
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-21 21:40:40 +0100

    add exercise 23

---
+++ week-02/day-02/exercise1-35/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Zita on 2017. 03. 21..
+ */
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+    int a = 0;
+    while (a < 100) {
+      System.out.println("I won't cheat on the exam!");
+      a += 1;
+    }
+  }
+}
