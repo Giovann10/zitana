df703b7579d6bca90988932f31000dded0a2f979
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-22 20:30:05 +0100

    add CompareLength exercise

---
+++ week-02/day-03/FunctionsAndArrays/src/CompareLength.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Zita on 2017. 03. 22..
+ */
+public class CompareLength {
+  public static void main(String[] args) {
+    int[] p1 = {1 , 2 , 3};
+    int[] p2 = {4 , 5};
+  
+    if (p1.length < p2.length) {
+      System.out.println("`p2` has more elements than `p1`");
+    }
+  }
+}
