f1576ef6bb2d3bbb5af6c602dc0d10434688ce0c
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-22 20:11:26 +0100

    add AppendA exercise

---
+++ week-02/day-03/FunctionsAndArrays/src/AppendA.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Zita on 2017. 03. 22..
+ */
+public class AppendA {
+  public static void main(String[] args) {
+    String am = "kuty";
+    appendA(am);
+  }
+  
+  public static void appendA (String a) {
+    System.out.println(a + "a");
+  }
+}
