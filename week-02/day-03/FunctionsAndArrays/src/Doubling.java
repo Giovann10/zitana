8111f8970c6788e3405e86abe3026481e287c2cc
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-22 19:54:41 +0100

    day-03 dir created

---
+++ week-02/day-03/FunctionsAndArrays/src/Doubling.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Zita on 2017. 03. 22..
+ */
+public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+    doubling(ak);
+  }
+  
+  public static void doubling (int input){
+    System.out.println(input * 2);
+  }
+}
