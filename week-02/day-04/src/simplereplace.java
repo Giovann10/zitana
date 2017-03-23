81db84525703282a3eace05e15b5cc9d88b70fe8
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-23 11:53:31 +0100

    (workshop exercises): add simplereplace.java

---
+++ week-02/day-04/src/simplereplace.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Zita on 2017. 03. 23..
+ */
+public class simplereplace {
+  public static void main(String[] args) {
+    String example = "In a dishwasher far far away";
+  
+    System.out.println(example.replace("dishwasher" , "galaxy"));
+    
+  }
+}
