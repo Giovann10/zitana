ef3b12ca16348e935949966ce529071306609e18
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-24 11:00:03 +0100

    add for loop in GuessMyNumber

--- week-02/day-05/Projects/src/GuessMyNumber.java
+++ week-02/day-05/Projects/src/GuessMyNumber.java
@@ -8,23 +8,40 @@ public class GuessMyNumber {
     Scanner scanner = new Scanner(System.in);
     int range = scanner.nextInt();
     System.out.println("Okay! I am thinking of a number between 1 and " + range + ".");
-    
+  
     for (int i = 0; i < 3; i++) {
       System.out.print(".");
       TimeUnit.SECONDS.sleep(1);
     }
-    
-    int myNumber = randInt(1 , range);
-    
+  
+    int myNumber = randInt(1, range);
+  
     System.out.println("\nGot it!");
-    TimeUnit.SECONDS.sleep(2);
+    TimeUnit.SECONDS.sleep(1);
     System.out.println("You can quess 5 times.\nEnter your first guess:");
+  
+    int guess = scanner.nextInt();
+  
+    for (int i = 5; i > 0; i--) {
+      if (guess == myNumber) {
+        System.out.println("That is correct! My number is " + myNumber + " indeed.");
+        break;
+      } else if (guess < myNumber) {
+        System.out.println("Too low! You have " + i + " lives left. Try again:");
+        guess = scanner.nextInt();
+      } else if (guess > myNumber) {
+        System.out.println("Too high! You have " + i + " lives left. Try again:");
+        guess = scanner.nextInt();
+      } else if (guess != myNumber && i == 1){
+        System.out.println("You have run out of lives.\n :(\nMy number was: " + myNumber);
+      }
+    }
   }
   
   public static int randInt(int min, int max) {
-    Random rand;
-    int randomNum = rand.nextInt((max - min) + 1) + min;
-  
-    return randomNum;
+    Random rand = new Random();
+    int randNum = rand.nextInt(max - min + 1) + min;
+    return randNum;
   }
+  
 }
