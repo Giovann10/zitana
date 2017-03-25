84ed69f7f057aee307dfc9b2a5770909adad705c
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-25 19:07:52 +0100

    add day-02 exercise 30

---
+++ week-02/day-02/exercise1-35/src/DrawSquare.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DrawSquare {
+  public static void main(String[] args) {
+    System.out.println("Enter a number: ");
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    
+    for (int i = 0 ; i < n+1; i++) {
+      System.out.println();
+      if (i == 0 || i == n) {
+        for (int j = 0; j < n; j++) {
+          System.out.print("%");
+        }
+      } else {
+          for (int k = 0; k < n; k++) {
+            if (k == 0 || k == n-1) {
+              System.out.print("%");
+            } else {
+              System.out.print(" ");
+            }
+          }
+        }
+      }
+    }
+  }
