52ef39142a555ac5cf85ec658c6e508ff30d9d81
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-26 11:37:49 +0200

    add exercise 35 to day-02

---
+++ week-02/day-02/exercise1-35/src/DrawChessTable.java
@@ -0,0 +1,18 @@
+public class DrawChessTable {
+  public static void main(String[] args) {
+    int a = 8;
+    
+    for (int i=0; i <a; i++) {
+      if (i % 2 == 0) {
+        for (int j = 0; j < a/2; j++) {
+          System.out.print("% ");
+        }
+      }else {
+        for (int k = 0 ; k < a/2; k++) {
+          System.out.print(" %");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
