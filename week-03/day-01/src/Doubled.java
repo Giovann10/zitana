15e6c4c8e8b65e60ed8678b87652d769e9c82588
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-28 21:14:34 +0200

    (refactor):make separate functions

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -1,8 +1,10 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Doubled {
@@ -10,23 +12,28 @@ public class Doubled {
     Path filePath = Paths.get("duplicated-chars.txt");
     try {
       List<String> lines = Files.readAllLines(filePath);
-      
-      List<String> simpleChar = new ArrayList<String>();
-  
-      for (String row : lines) {
-        String toAdd = "";
-        char [] charLines = row.toCharArray();
-          for (int i= 0 ; i < charLines.length; i += 2) {
-            toAdd += charLines[i];
-          }
-        System.out.println(toAdd);
-        simpleChar.add(toAdd);
-      }
-      System.out.println(simpleChar);
-  
-  
+      printer(removeDoubledChar(lines));
     } catch (IOException e) {
-      System.out.println("error");
+      System.out.println("Couldn't read/initialize file");
+    }
+  }
+  
+  public static List<String> removeDoubledChar (List<String> doubleLines) {
+    List<String> simpleChar = new ArrayList<>();
+    for (String row : doubleLines) {
+      String toAdd = "";
+      char[] charLines = row.toCharArray();
+      for (int i = 0; i < charLines.length; i += 2) {
+        toAdd += charLines[i];
+      }
+      simpleChar.add(toAdd);
+    }
+    return simpleChar;
+  }
+  
+  public static void printer (List<String> linesToPrint) {
+    for (String row : linesToPrint) {
+      System.out.println(row);
     }
   }
 }
