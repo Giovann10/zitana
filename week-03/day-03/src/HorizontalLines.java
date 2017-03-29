b00bca56ebdf4371dfa19211a8806480d226d072
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-29 11:04:30 +0200

    (ws exercises): rename function

--- week-03/day-03/src/HorizontalLines.java
+++ week-03/day-03/src/HorizontalLines.java
@@ -2,24 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class GoToCenter {
-  
+public class HorizontalLines {
   public static void mainDraw(Graphics graphics){
     // create a line drawing function that takes 2 parameters:
     // the x and y coordinates of the line's starting point
-    // and draws a line from that point to the center of the canvas.
+    // and draws a 50 long horizontal line from that point.
     // draw 3 lines with that function.
-  
-    randomLines(graphics);
-    randomLines(graphics);
-    randomLines(graphics);
+    
+    
+    
   }
   
-  public static void randomLines (Graphics graphics) {
-    graphics.setColor(Color.RED);
-    graphics.drawLine ((int)(Math.random()*300),(int)(Math.random()*300),150,150);
-  }
-  
+  //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(300, 300));
@@ -33,6 +27,7 @@ public class GoToCenter {
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
       mainDraw(graphics);
+      
     }
   }
 }
