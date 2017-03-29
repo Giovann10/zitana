3257d90de92fe33e1c5a59d8d12407166c7700c4
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-29 21:13:40 +0200

    (ws exercises): add box

--- week-03/day-03/src/ConnectTheDots.java
+++ week-03/day-03/src/ConnectTheDots.java
@@ -14,8 +14,10 @@ public class ConnectTheDots {
     // connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
     // {120, 100}, {85, 130}, {50, 100}}
     
+    int[][] box = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
     int[][] dotsToConnect = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
     
+    connect(box,graphics);
     connect(dotsToConnect, graphics);
     
   }
