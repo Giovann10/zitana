cb5d9c07505f9229947024d483647a15dacb990e
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-29 10:53:23 +0200

    (ws exercises): add 04 GoToCenter

---
+++ week-03/day-03/src/GoToCenter.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+  
+  public static void mainDraw(Graphics graphics){
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // draw 3 lines with that function.
+    
+    drawLine(graphics);
+    drawLine(graphics);
+    drawLine(graphics);
+  }
+  
+  public static void drawLine (Graphics graphics) {
+    graphics.setColor(Color.RED);
+    graphics.drawLine ((int)(Math.random()*300),(int)(Math.random()*300),150,150);
+  }
+  
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
