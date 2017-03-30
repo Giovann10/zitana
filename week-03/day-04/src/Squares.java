8180d80dc8d26690b00b92208bb568925bee25b1
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-03-30 15:56:37 +0200

    (ws exercises) add Square fractal almost correct code

---
+++ week-03/day-04/src/Squares.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Squares {
+  public static void mainDraw(Graphics graphics) {
+    int startX = 0;
+    int startY = 0;
+    int size = 300;
+    
+    squares(startX, startY, size, graphics);
+  }
+  
+  public static void squares (int x, int y, int size, Graphics graphics) {
+    graphics.drawRect(x, y, size, size);
+    
+    if (size <= 10) {
+      return;
+      
+    } else {
+      squares( x, y + size / 3, size / 3, graphics);
+      squares(x + size / 3, y, size / 3, graphics);
+      squares(x + size / 3, y + 2 * size / 3, size / 3, graphics);
+      squares(x + 2 * size / 3, y + size / 3, size / 3, graphics);
+    }
+  }
+  
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      super.setBackground(Color.YELLOW);
+      mainDraw(graphics);
+      
+    }
+  }
+}
+
