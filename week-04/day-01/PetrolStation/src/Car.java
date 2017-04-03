cd071a5c527dc340da475865821ce0e82e9c8418
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-03 15:50:36 +0200

    (workshop exercises) add PetrolStation

---
+++ week-04/day-01/PetrolStation/src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+  public int gasAmount;
+  public int capacity;
+  
+  public Car() {
+    gasAmount = 0;
+    capacity = 100;
+  }
+}
---
+++ week-04/day-01/PetrolStation/src/Station.java
@@ -0,0 +1,8 @@
+public class Station {
+  public int gasAmount;
+  
+  public void refill(Car car) {
+    gasAmount -= car.capacity;
+    car.gasAmount = car.capacity;
+  }
+}
