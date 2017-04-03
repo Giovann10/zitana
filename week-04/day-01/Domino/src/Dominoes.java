c67538fe75afb6864dbf1fbad056717212e88011
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-03 13:40:32 +0200

    (ws exercises) add Dominoes

---
+++ week-04/day-01/Domino/src/Dominoes.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Dominoes {
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+    
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+    
+    System.out.println(snake(dominoes));
+  }
+  
+  public static List<Domino> snake (List<Domino> dominoes) {
+    for (int i = 0; i < dominoes.size() - 1; i++) {
+      int a = dominoes.get(i).getValues()[1];
+      for (int j = i + 1; j < dominoes.size(); j++){
+        int b = dominoes.get(j).getValues()[0];
+        if (a == b) {
+          Collections.swap(dominoes, i + 1, j);
+        }
+      }
+    }
+    return dominoes;
+  }
+  
+  static List<Domino> initializeDominoes() {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+    return dominoes;
+  }
+}
