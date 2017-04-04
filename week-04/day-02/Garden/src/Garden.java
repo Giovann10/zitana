f8247e109208107a659369adfbd2ef56e42d5792
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-04 17:42:42 +0200

    remove GardenApp, make static functions in Garden, printer still need to be fixed

--- week-04/day-02/Garden/src/Garden.java
+++ week-04/day-02/Garden/src/Garden.java
@@ -2,21 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Garden {
-  private List<Plant> plants = new ArrayList<>();
+  public static void main(String[] args) {
   
-  public List<Plant> getPlants() {
-    return plants;
+   ArrayList<Plant> garden = new ArrayList<>();
+  
+    Plant yellowFlower = new Flower("yellow");
+    garden.add(yellowFlower);
+    Plant blueFlower = new Flower("blue");
+    garden.add(blueFlower);
+    Plant purpleTree = new Tree("purple");
+    garden.add(purpleTree);
+    Plant orangeTree = new Tree("orange");
+    garden.add(orangeTree);
+  
+    printer(garden);
+    needWater(garden);
+    watering(40, needWater(garden));
+    printer(garden);
+    needWater(garden);
+    watering(70, needWater(garden));
+    printer(garden);
+  
   }
   
-  public void setPlants(List<Plant> plants) {
-    this.plants = plants;
-  }
-  
-  public void addPlant(Plant p) {
-    plants.add(p);
-  }
-  
-  public ArrayList<Plant> needWater(ArrayList<Plant> plants) {
+  public static ArrayList<Plant> needWater(ArrayList<Plant> plants) {
     ArrayList<Plant> plantsToWater = new ArrayList<>();
     
     for (int i = 0; i < plants.size(); i++) {
@@ -27,13 +36,21 @@ public class Garden {
     return plantsToWater;
   }
   
-  public void watering(double amountOfWater, List<Plant> plantsToWater) {
+  public static void watering(double amountOfWater, ArrayList<Plant> plantsToWater) {
     for (int i = 0; i < plantsToWater.size(); i++) {
       plantsToWater.get(i).absorb(amountOfWater / plantsToWater.size());
     }
     System.out.println("Watering with" + amountOfWater);
   }
   
-  
+  public static void printer (ArrayList<Plant> garden) {
+    for (int i = 0; i < garden.size(); i++) {
+      if (garden.get(i).getThreshold() > garden.get(i).getWaterLevel()) {
+        System.out.println(garden.get(i) + " needs water.");
+      } else {
+        System.out.println(garden.get(i) + " doesn't need water.");
+      }
+    }
+  }
   
 }
