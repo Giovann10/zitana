6fd0aa2c76985034af7e67c6801adfc240955873
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-11 12:33:50 +0200

    add tile coordinates, index error need to be fixed

--- week-05/RPG/src/Board.java
+++ week-05/RPG/src/Board.java
@@ -5,13 +5,14 @@ import java.awt.event.KeyListener;
 
 public class Board extends JComponent implements KeyListener {
   
-  int testBoxX;
-  int testBoxY;
+  int heroBoxX;
+  int heroBoxY;
   String heroImage;
+  int[][] map = {{0, 0, 0, 1, 0, 1, 0, 0, 0, 0},{0, 0, 0, 1, 0, 1, 0, 1, 1, 0},{0, 1, 1, 1, 0, 1, 0, 1, 1, 0},{0,0,0,0,0,1,0,0,0,0},{1,1,1,1,0,1,1,1,1,0},{0,1,0,1,0,0,0,0,1,0},{0,1,0,1,0,1,1,0,1,0},{0,0,0,0,0,1,1,0,1,0},{0,1,1,1,0,0,0,0,1,0},{0,0,0,1,0,1,1,0,1,0},{0,1,0,1,0,1,0,0,0,0}};
   
   public Board() {
-    testBoxX = 0;
-    testBoxY = 0;
+    heroBoxX = 0;
+    heroBoxY = 0;
     heroImage = "hero-down.png";
     
     // set the size of your draw board
@@ -24,8 +25,7 @@ public class Board extends JComponent implements KeyListener {
     super.paint(graphics);
     // here you have a 720x720 canvas
     // you can create and draw an image using the class below e.g.
-  
-    int[][] map = {{0, 0, 0, 1, 0, 1, 0, 0, 0, 0},{0, 0, 0, 1, 0, 1, 0, 1, 1, 0},{0, 1, 1, 1, 0, 1, 0, 1, 1, 0},{0,0,0,0,0,1,0,0,0,0},{1,1,1,1,0,1,1,1,1,0},{0,1,0,1,0,0,0,0,1,0},{0,1,0,1,0,1,1,0,1,0},{0,0,0,0,0,1,1,0,1,0},{0,1,1,1,0,0,0,0,1,0},{0,0,0,1,0,1,1,0,1,0},{0,1,0,1,0,1,0,0,0,0}};
+    
     for (int j = 0; j < map.length; j++) {
       for (int i = 0; i < map[j].length; i++) {
         if (map[j][i] == 1) {
@@ -37,7 +37,7 @@ public class Board extends JComponent implements KeyListener {
         }
       }
   
-      PositionedImage hero = new PositionedImage(heroImage, testBoxX, testBoxY);
+      PositionedImage hero = new PositionedImage(heroImage, heroBoxX, heroBoxY);
       hero.draw(graphics);
     }
   }
@@ -56,18 +56,24 @@ public class Board extends JComponent implements KeyListener {
   // But actually we can use just this one for our goals here
   @Override
   public void keyReleased(KeyEvent e) {
+    
+    int tileRight = map[(heroBoxX + 72) / 72][heroBoxY / 72];
+    int tileLeft = map[(heroBoxX - 72) / 72][heroBoxY / 72];
+    int tileUp = map[heroBoxX / 72][(heroBoxY - 72) / 72];
+    int tileDown = map[heroBoxX / 72][(heroBoxY + 72) / 72];
+    
     // When the up or down keys hit, we change the position of our box
-      if (e.getKeyCode() == KeyEvent.VK_UP && testBoxY > 0) {
-        testBoxY -= 72;
+      if (e.getKeyCode() == KeyEvent.VK_UP && heroBoxY > 0 && tileUp == 0) {
+        heroBoxY -= 72;
         heroImage = "hero-up.png";
-      } else if (e.getKeyCode() == KeyEvent.VK_DOWN && testBoxY < 720) {
-        testBoxY += 72;
+      } else if (e.getKeyCode() == KeyEvent.VK_DOWN && heroBoxY < 720 && tileDown == 0) {
+        heroBoxY += 72;
         heroImage = "hero-down.png";
-      } else if (e.getKeyCode() == KeyEvent.VK_LEFT && testBoxX > 0) {
-        testBoxX -= 72;
+      } else if (e.getKeyCode() == KeyEvent.VK_LEFT && heroBoxX > 0 && tileLeft == 0) {
+        heroBoxX -= 72;
         heroImage = "hero-left.png";
-      } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && testBoxX < 648) {
-        testBoxX += 72;
+      } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && heroBoxX < 648 && tileRight == 0) {
+        heroBoxX += 72;
         heroImage = "hero-right.png";
       }
       // and redraw to have a new picture with the new coordinates
