a2609b1caa37b232b1b40d6719f4d54ad0851647
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-30 12:45:03 +0200

    finish lotteryfilter

--- week-06/day-04/lotteryfilter/src/main/java/LotteryFilter.java
+++ week-06/day-04/lotteryfilter/src/main/java/LotteryFilter.java
@@ -3,21 +3,39 @@ import com.opencsv.CSVWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 public class LotteryFilter {
 
+  private static String source = "otos.csv";
+  private static String destination = "output.csv";
+
   public static void main(String[] args) {
+    OptionParser parser = new OptionParser();
+    parser.accepts("y").withRequiredArg();
+    parser.accepts("f").withRequiredArg();
+    parser.accepts("o").withRequiredArg();
 
-    try {
-      readFile("otos.csv");
-    } catch (IOException e) {
-      System.out.println("File could not be read");
+    OptionSet options = parser.parse(args);
+    
+    if (options.has("y")) {
+      if (options.has("f")) {
+        source = options.valueOf("f").toString();
+      }
+      if (options.has("o")) {
+        destination = options.valueOf("o").toString();
+      }
+      try {
+        writeFile(destination, filterByYear(options.valueOf("y").toString(),readFile(source)));
+      }catch (IOException e) {
+        System.out.println("File could not be read");
+      }
+    } else {
+      System.out.println("Please include the year to filter");
     }
-
-  }
-
-  public static void initialise(String[] args) {
 
   }
 
@@ -26,10 +44,19 @@ public class LotteryFilter {
     return reader.readAll();
   }
 
-  public void writeFile(String fileName, List<String> lines) throws IOException {
-    CSVWriter writer = new CSVWriter(new FileWriter(fileName), ';',
-        CSVWriter.NO_QUOTE_CHARACTER);
+  public static void writeFile(String fileName, List<String[]> lines) throws IOException {
+    CSVWriter writer = new CSVWriter(new FileWriter(fileName), ';', CSVWriter.NO_QUOTE_CHARACTER);
     writer.writeAll(lines);
     writer.close();
+  }
+
+  public static List<String[]> filterByYear(String year, List<String[]> lines) {
+    List<String[]> filteredLines = new ArrayList<>();
+    for (String[] line : lines) {
+      if (line[0].equals(year)) {
+        filteredLines.add(line);
+      }
+    }
+    return filteredLines;
   }
 }
