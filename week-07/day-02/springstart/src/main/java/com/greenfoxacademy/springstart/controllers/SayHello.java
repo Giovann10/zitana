6a4ee0c13bab78599083a697b41c49ae3fd11652
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-02 16:50:48 +0200

    add sayhello without colors

--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/SayHello.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/SayHello.java
@@ -1,5 +1,8 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SayHello {
-  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+  List<String> hellos = new ArrayList<>(Arrays.asList("Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
       "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
       "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"));
 
   @RequestMapping("/web/hello")
   public String greeting(Model model) {
