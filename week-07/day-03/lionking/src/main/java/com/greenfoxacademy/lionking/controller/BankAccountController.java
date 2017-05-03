a7076f42c91c636f79ba79a9357590f36397fe8c
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-03 14:49:25 +0200

    add exercise 5

--- week-07/day-03/lionking/src/main/java/com/greenfoxacademy/lionking/controller/BankAccountController.java
+++ week-07/day-03/lionking/src/main/java/com/greenfoxacademy/lionking/controller/BankAccountController.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.lionking.controller;
 
 import com.greenfoxacademy.lionking.model.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +16,15 @@ public class BankAccountController {
     return "account";
   }
 
+  @RequestMapping("/exercise5")
+  public String displayAccounts(Model model) {
+    List<BankAccount> bankAccounts = new ArrayList<>();
+    bankAccounts.add(new BankAccount("Simba", 2000, "lion"));
+    bankAccounts.add(new BankAccount("Timon", 200, "meerkat"));
+    bankAccounts.add(new BankAccount("Pumbaa", 0, "warthhog"));
+    bankAccounts.add(new BankAccount("Scar", 5000, "lion"));
+    bankAccounts.add(new BankAccount("Rafiki", 1000, "mandrill"));
+    model.addAttribute("accounts", bankAccounts);
+    return "bankaccounts";
+  }
 }
