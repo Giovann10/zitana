ac8b95021d570b6410f23796125ec3e33f953ab2
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-09 14:03:28 +0200

    populate database

--- week-08/day-02/listtodos/src/main/java/com/greenfoxacademy/listtodos/ListtodosApplication.java
+++ week-08/day-02/listtodos/src/main/java/com/greenfoxacademy/listtodos/ListtodosApplication.java
@@ -1,12 +1,26 @@
 package com.greenfoxacademy.listtodos;
 
+import com.greenfoxacademy.listtodos.model.Todo;
+import com.greenfoxacademy.listtodos.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ListtodosApplication {
+public class ListtodosApplication implements CommandLineRunner {
+
+		@Autowired
+		TodoRepository todoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListtodosApplication.class, args);
 	}
+
+  @Override
+  public void run(String... args) throws Exception {
+    todoRepo.save(new Todo("I have to learn Object Relational Mapping"));
+    todoRepo.save(new Todo("I have to master Spring Boot"));
+
+  }
 }
