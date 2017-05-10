b2465efbb894ebc7f0c72f3091c4e360d2ce8d63
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-10 11:11:17 +0200

    add exercise 01

--- week-08/day-03/workshop/src/main/java/com/greenfoxacademy/workshop/controller/MainController.java
+++ week-08/day-03/workshop/src/main/java/com/greenfoxacademy/workshop/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.greenfoxacademy.workshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
+
+  @RequestMapping("/")
   public String loadIndex() {
     return "index";
   }
