df9cfa913591a30851313a6f915ecce9c5f84f5e
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-05 15:57:29 +0200

    add some tests

--- week-10/caloriecounterproject/src/test/java/com/greenfoxacademy/calorie/controller/MainRestControllerTest.java
+++ week-10/caloriecounterproject/src/test/java/com/greenfoxacademy/calorie/controller/MainRestControllerTest.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.calorie.controller;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenfoxacademy.calorie.CaloriecounterApplication;
 import com.greenfoxacademy.calorie.model.Meal;
@@ -16,16 +22,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import static javax.management.Query.value;
-import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 
 @RunWith(SpringRunner.class)
@@ -43,8 +41,8 @@ public class MainRestControllerTest {
   @Autowired
   private WebApplicationContext webApplicationContext;
 
-  @Autowired
-  MealRepository mealRepository;
+//  @Autowired
+//  MealRepository mealRepository;
 
   @Before
   public void setup() throws Exception {
@@ -52,28 +50,55 @@ public class MainRestControllerTest {
   }
 
   @Test
-  public void getmMeals_getFirst_Bacon() throws Exception {
+  public void getmMeals_First_Element_Type_Bacon() throws Exception {
     mockMvc.perform(get("/getmeal")
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(content().contentType(contentType))
         .andExpect(jsonPath("$[0].type", is("Breakfast")))
         .andExpect(jsonPath("$[0].description", is("bacon")))
-        .andExpect(jsonPath("$[3].type", is("Midnight Snack")));
+        .andExpect(jsonPath("$[2].calories", is(650)))
+        .andExpect(jsonPath("$[3].type", is("Midnight Snack")))
+//        .andExpect(jsonPath("$[3].date", is("2017-06-05")));
+  }
+  @Test
+  public void getStats_4_1200() throws Exception {
+  mockMvc.perform(get("/getstats")
+        .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(contentType))
+      .andExpect(jsonPath("$.numberOfMeals", is(4)))
+      .andExpect(jsonPath("$.calories", is(1200)));
   }
 
-
   @Test
-  public void postMeal() throws Exception {
+  public void postMeal_Dinner_Cucumber_200() throws Exception {
     Meal meal = new Meal("Dinner", "cucumber", 200);
     ObjectMapper mapper = new ObjectMapper();
     String jsonInput = mapper.writeValueAsString(meal);
 
-    mockMvc.perform(post("/postMeal")
+    mockMvc.perform(post("/meal")
         .contentType(contentType)
         .content(jsonInput))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.type", is("Dinner")));
+        .andExpect(jsonPath("$.type", is("Dinner")))
+        .andExpect(jsonPath("$.description", is("cucumber")))
+        .andExpect(jsonPath("$.calories", is(200)));
+  }
+
+  @Test
+  public void putMeal_Dinner_Cucumber_200() throws Exception {
+    Meal meal = new Meal("Dinner", "cucumber", 200);
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonInput = mapper.writeValueAsString(meal);
+
+    mockMvc.perform(post("/meal")
+        .contentType(contentType)
+        .content(jsonInput))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.type", is("Dinner")))
+        .andExpect(jsonPath("$.description", is("cucumber")))
+        .andExpect(jsonPath("$.calories", is(200)));
   }
 
 }
